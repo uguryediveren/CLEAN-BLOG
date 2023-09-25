87d75e92d82823ab7a00da13e831e534eca664cb
Hamdi Uğur Yediveren <ugur_yed@hotmail.com>
2023-09-25 17:42:50 +0300

    routes and template engines added
    routes and template engines added-forDescription

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
 
-const PORT = 3000;
+const PORT = 3001;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.send(blog);
+  res.render('index');
+  console.log('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+  console.log('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+  console.log('add_post');
 });
 
 app.listen(PORT, () => {
