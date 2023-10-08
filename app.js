965ea888e6e22c8f8032bf168ebae7fdec031ad2
Hamdi Uğur Yediveren <ugur_yed@hotmail.com>
2023-10-08 17:45:51 +0300

    post bilgileri ile view sayfasında renderlandı

--- app.js
+++ app.js
@@ -30,6 +30,13 @@ app.post('/posts', async (req, res) => {
   res.redirect('/');
 });
 
+app.get('/posts/:id', async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.render('post', {
+    post,
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
